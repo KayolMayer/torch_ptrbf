4e8345f7b871634f2a911be93cb37661f604d3fd
KayolMayer <kayol@outlook.com.br>
2025-03-18 13:13:36 +0100

    bug_correction_training_eval

--- main.py
+++ main.py
@@ -3,28 +3,104 @@ Created on Wed Mar 12 16:22:22 2025.
 
 @author: kayol
 """
-import torch
-import torch.nn as nn
+from os import sep, getcwd
+from torch import no_grad, optim, manual_seed, linspace, tensor, mean
+from torch.cuda import is_available
+from torch.nn import Module, Sequential
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
-from torchptrbf.layers   import vanilla
-from torchptrbf.loss     import mse_loss
+from numpy.random import seed
+
+from torchptrbf.layers import vanilla
+from torchptrbf.loss import mse_loss, calc_loss_loader
 from torchptrbf.datasets import beamforming_dataset_loader
-
-from torch import sum
 
 import matplotlib.pyplot as plt
 
+# PT-RBF parameters
+ptrbf_arc = {
+    'inputs': 6,
+    'outputs': [50, 3],
+    'neurons': [50, 50],
+    'batch': 1,
+    'lr': 3e-3,
+    'epochs': 20,
+    'len_train': 1e4,
+    'len_val': 1e3,
+    'len_inf': 1e4,
+    'random_seed': 123
+    }
 
-class ptrbf_nn(nn.Module):
+def plot_losses(num_epochs, train_losses, val_losses):
+    """
+    Plot training and validation losses against epochs and data seen.
+
+    This function generates a loss curve with two x-axes:
+    - The primary x-axis represents epochs.
+    - The secondary x-axis represents the number of tokens seen.
+    The plot includes both training and validation losses.
+
+    Returns
+    -------
+        num_epochs (int): Number of training epochs.
+        train_losses (list of float): Training losses.
+        val_losses (list of float): Validation losses.
+
+    Parameters
+    ----------
+        None: The function saves the plot as 'loss-plot.pdf' and displays it.
+    """
+    folder = getcwd() + sep + "results" + sep
+
+    epochs_seen = linspace(0, num_epochs, len(train_losses))
+
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+
+    # Plot training and validation loss against epochs
+    ax1.plot(epochs_seen, train_losses, label="Training loss")
+    ax1.plot(epochs_seen, val_losses, linestyle="-.", label="Validation loss")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.set_axisbelow(True)
+    ax1.yaxis.grid(color='gray', linestyle='dashed')
+
+    # only show integer labels on x-axis
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+
+    fig.tight_layout()  # Adjust layout to make room
+    plt.savefig(folder + "loss-plot.pdf")
+    plt.show()
+
+
+def plot_consts(const, name):
+    folder = getcwd() + sep + "results" + sep
+
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+
+    ax1.scatter(tensor(const).real, tensor(const).imag)
+
+    ax1.set_xlabel("I")
+    ax1.set_ylabel("Q")
+    ax1.set_axisbelow(True)
+    ax1.yaxis.grid(color='gray', linestyle='dashed')
+
+    fig.tight_layout()  # Adjust layout to make room
+    plt.savefig(folder + name + ".pdf")
+    plt.show()
+
+
+class ptrbf_nn(Module):
 
     def __init__(self, inputs, neurons, outputs):
         super().__init__()
 
         input_vec = [inputs] + outputs[:-1]
 
-        self.layers = nn.Sequential(*[vanilla(i, n, o)
-                                      for i, n, o in zip(input_vec,
-                                                         neurons, outputs)])
+        self.layers = Sequential(*[vanilla(i, n, o)
+                                   for i, n, o in zip(input_vec,
+                                                      neurons, outputs)])
 
     def forward(self, x):
 
@@ -32,123 +108,133 @@ class ptrbf_nn(nn.Module):
 
         return y
 
-def train(dataset, model, loss_fn, optimizer, device):
 
-    size = len(dataset)
-    model.train()
-    for batch, (X, y) in enumerate(dataset):
-        X, y = X.to(device), y.to(device)
+def train(model, train_loader, val_loader, optimizer, device, num_epochs):
 
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(y, pred)
+    # Initialize lists to track losses.
+    train_losses, val_losses = [], []
 
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
+    # Initialize lists to track losses.
+    train_losses_batch, val_losses_batch = [], []
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    print('Training PTRBF...')
 
-def test(dataset, model, loss_fn, device):
-    size = len(dataset)
-    num_batches = dataset.batch_size
+    # Main training loop.
+    for epoch in range(num_epochs):
+
+        print(f"Epoch {epoch+1}:")
+
+        # Set model to training mode.
+        model.train()
+
+        for input_batch, target_batch in train_loader:
+
+            # Reset loss gradients from previous batch iteration
+            optimizer.zero_grad()
+
+            # Pass data to device
+            input_batch = input_batch.to(device)
+            target_batch = target_batch.to(device)
+
+            # Compute prediction error
+            pred = model(input_batch)
+            loss = mse_loss(target_batch, pred)
+
+            # Calculate loss gradients
+            loss.backward()
+            # Update model weights using loss gradients
+            optimizer.step()
+
+        train_loss, val_loss = evaluate_model(model, train_loader,
+                                              val_loader, device)
+
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
+
+        print(f"Train loss {train_losses[-1]:.3f}, Val loss {val_losses[-1]:.3f}")
+
+    return train_losses, val_losses
+
+
+def evaluate_model(model, train_loader, val_loader, device):
+
     model.eval()
-    test_loss, error = 0, 0
-    with torch.no_grad():
-        for X, y in dataset:
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            test_loss += loss_fn(y, pred).item()
-            error += sum(sum(abs(y - pred))).item()
 
-    test_loss /= num_batches
-    error /= (size*num_batches)
-    #plt.scatter(pred[:,2,0].real,pred[:,2,0].imag)
-    print(f"Test Error: {error:>8f}, Avg loss: {test_loss:>8f} \n")
+    with no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device)
+        val_loss = calc_loss_loader(val_loader, model, device)
+    model.train()
+
+    return train_loss, val_loss
 
 
-batch_size = 1
-len_data   = 1e4
-epochs     = 20
+def inference(inference_loader, model, device):
+
+    model.eval()
+
+    qam4 = []
+    psk8 = []
+    qam16 = []
+
+    with no_grad():
+
+        for input_batch, _ in inference_loader:
+
+            # Pass data to device
+            input_batch = input_batch.to(device)
+
+            # Compute prediction error
+            pred = model(input_batch)
+
+            qam4.append(pred[0, 0, 0])
+            psk8.append(pred[0, 0, 1])
+            qam16.append(pred[0, 0, 2])
+
+    model.train()
+
+    return qam4, psk8, qam16
+
+
+# Seed of random numbers numpy
+seed(ptrbf_arc['random_seed'])
 
 # Create data loaders
-train_dataloader = beamforming_dataset_loader(len_data=len_data, batch_size=batch_size, shuffle=True)
-test_dataloader  = beamforming_dataset_loader(len_data=len_data, batch_size=batch_size)
+train_loader = beamforming_dataset_loader(len_data=ptrbf_arc['len_train'],
+                                          batch_size=ptrbf_arc['batch'],
+                                          shuffle=True)
+
+val_loader = beamforming_dataset_loader(len_data=ptrbf_arc['len_val'],
+                                        batch_size=1)
+
+inf_loader = beamforming_dataset_loader(len_data=ptrbf_arc['len_inf'],
+                                        batch_size=1)
 
 # Get device for training
-#device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+#device = "cuda" if is_available() else "cpu"
 device = 'cpu'
 print(f"Using {device} device")
 
+# For reproducibility due to the shuffling in the data loader.
+manual_seed(ptrbf_arc['random_seed'])
 
-inputs  = 6
-outputs = [50, 3]
-neurons = [50, 50]
-ptrbf = ptrbf_nn(inputs, neurons, outputs).to(device)
-print(ptrbf)
-
-loss_fn = mse_loss
-optimizer = torch.optim.SGD(ptrbf.parameters(), lr=3e-3)
+ptrbf = ptrbf_nn(ptrbf_arc['inputs'], ptrbf_arc['neurons'],
+                 ptrbf_arc['outputs']).to(device)
+ptrbf.eval()
 
 
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, ptrbf, loss_fn, optimizer, device)
-    test(test_dataloader, ptrbf, loss_fn, device)
-print("Done!")
+optimizer = optim.SGD(ptrbf.parameters(), lr=ptrbf_arc['lr'])
 
+# Model training
+train_losses, val_losses = train(
+    ptrbf, train_loader, val_loader, optimizer, device,
+    num_epochs=ptrbf_arc['epochs'])
 
+# Plot the training and validation losses.
+plot_losses(ptrbf_arc['epochs'], train_losses, val_losses)
 
+# Model inference
+qam4, psk8, qam16 = inference(inf_loader, ptrbf, device)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# inputs  = 5
-# outputs = [10, 10, 10]
-# neurons = [5, 6, 7]
-
-# ptrbf = ptrbf_nn(inputs, neurons, outputs)
-
-# x      = torch.randn(1,inputs, 1)+torch.randn(1,inputs, 1)*1j
-# target = torch.ones(1, outputs[-1], 1)+torch.ones(1, outputs[-1], 1)*1j
-
-# optimizer = torch.optim.Adam(ptrbf.parameters(), lr=1e-3)
-
-# for _ in range(200):
-
-#     # Forward pass
-#     output = ptrbf(x)
-
-#     # Calculate loss based on how close the target
-#     # and output are
-#     loss      = mse_loss(output, target)
-
-#     # Backward pass to calculate the gradients
-#     loss.backward()
-#     optimizer.step()
-#     optimizer.zero_grad()
-
-
-# # In[173]:
-
-
-# print(mse_loss(output, target))
+plot_consts(qam4, "QAM-4")
+plot_consts(psk8, "PSK-8")
+plot_consts(qam16, "QAM-16")
--- torchptrbf/loss.py
+++ torchptrbf/loss.py
@@ -28,3 +28,47 @@ def mse_loss(target, output):
         A scalar tensor representing the computed MSE loss.
     """
     return 0.5 * (abs(target - output) ** 2).sum(dim=2).mean(dim=0)
+
+
+def calc_loss_loader(data_loader, model, device):
+    """
+    Compute the average cross-entropy loss over multiple batches from a loader.
+
+    This function iterates through a specified number of batches from the given
+    data loader, computes the loss for each batch and returns the average loss.
+
+    Parameters
+    ----------
+        data_loader (torch.utils.data.DataLoader): The data loader providing
+                                                   batches of input and target
+                                                   tensors.
+        model (torch.nn.Module): The NN model.
+        device (torch.device): The device (CPU or GPU) on which to perform
+                               computations.
+    Returns
+    -------
+        float: The average loss.
+               Returns NaN if the data loader is empty.
+    """
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    else:
+        # Reduce the number of batches to match the total number of batches in
+        # the data loader
+        # if num_batches exceeds the number of batches in the data loader
+        num_batches = len(data_loader)
+
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+
+        # Pass data to device
+        input_batch = input_batch.to(device)
+        target_batch = target_batch.to(device)
+
+        # Compute prediction error
+        pred = model(input_batch)
+
+        loss = mse_loss(target_batch, pred)
+        total_loss += loss.item()
+
+    return total_loss / num_batches
